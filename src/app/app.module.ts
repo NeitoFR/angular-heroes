179573f7c1b6278fb1774b656fdc4f6efd54d15c
Houm <212682057@logon.ds.ge.com>
2018-08-30 17:48:40 +0200

    replace with http rest server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+//Angular module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule  } from '@angular/common/http';
+//Node Module
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+//Custom Module and Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
@@ -20,7 +25,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
